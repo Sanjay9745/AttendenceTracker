39a4ca4ca08422a4c93d7b88c48d22b6e023f514
Sanjay9745 <sanjaysanthosh919@gmail.com>
2024-04-20 14:03:00 +0530

    new commit

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = { origin: "http://localhost:3000" };
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(
   fileUpload({
